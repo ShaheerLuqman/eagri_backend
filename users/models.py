c16851d73c416a46cf26b7c0062eef2318f1330a
Shaheer Luqman <shaheer@retrocausal.ai>
2025-03-24 23:18:54 +0500

    User can be deleted

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 class User(AbstractUser):
     """Custom user model for EAgri application"""
@@ -10,11 +12,22 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     
-    # Fields that are required by default:
-    # - username
-    # - password
-    
     REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'phone_number']
     
+    class Meta:
+        db_table = 'users_user'
+        # Add this to ensure related objects are deleted
+        default_related_name = 'users'
+
+    def delete(self, *args, **kwargs):
+        # Delete auth tokens first
+        self.auth_token.all().delete()
+        super().delete(*args, **kwargs)
+    
     def __str__(self):
-        return self.username 
+        return self.username
+@receiver(pre_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    # This will delete the user's auth token
+    from rest_framework.authtoken.models import Token
+    Token.objects.filter(user=instance).delete()
