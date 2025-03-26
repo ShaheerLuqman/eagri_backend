2683d28d3cd0b97251920c7a5dd0da88c49a4b90
izWiz <izaan.shaikh.2004@gmail.com>
2025-03-26 10:15:09 +0500

    E-Loan apis completed

--- E_Loan/urls.py
+++ E_Loan/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CreateLoanRequestView, ListLoanRequestView, LoanRequestDetailView
 
 urlpatterns = [
-    path('e_loan/request/', CreateLoanRequestView.as_view(), name='create-loan'),
-    path('e_loan/get_loan/', ListLoanRequestView.as_view(), name='list-loans'),
-    path('e_loan/get_loan/<int:pk>/', LoanRequestDetailView.as_view(), name='loan-detail'),
+    path('request/', CreateLoanRequestView.as_view(), name='create-loan'),
+    path('get_loan/', ListLoanRequestView.as_view(), name='list-loans'),
+    path('get_loan/<int:pk>/', LoanRequestDetailView.as_view(), name='loan-detail'),
 ]
